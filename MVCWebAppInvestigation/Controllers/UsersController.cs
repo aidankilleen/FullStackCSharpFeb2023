29fe6c229bc5b38c30e6738c775e7f8669a10f47
Aidan Killeen <aidan.killeen@gmail.com>
2023-02-15 16:03:31 +0000

    end of day 3

---
+++ MVCWebAppInvestigation/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCWebAppInvestigation.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["title"] = "The User List Page";
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["title"] = "About User List";
+            return View();
+        }
+    }
+}
